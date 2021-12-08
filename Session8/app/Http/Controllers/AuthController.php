018d4c07064fd1f8dc88d6b6fe117e2d6829f55c
w3schools-test <test@w3schools.com>
2021-12-08 15:05:54 +0330

    3th commit

--- Session8/app/Http/Controllers/AuthController.php
+++ Session8/app/Http/Controllers/AuthController.php
@@ -3,8 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Role;
+use App\Models\User;
+use Exception;
 
 class AuthController extends Controller
 {
-    //
+    public function AddUser(Request $request){
+        $user=null;
+        try {
+            $user=Role::create([
+                "name" => $request->name,
+                "email" => $request->email,
+                "mobile" => $request->mobile,
+                "role_id" => $request->role_id,
+                "password" => Hash::make($request->password,)
+            ]);
+        } catch (Exception $exception) {
+            return response()->json([
+                'data' => $exception,
+                'msg' => 'failed'], 500);
+        }
+        $token = $user->createToken('token')->plainTextToken;
+        $response =['User' => $user,'token'=>$token];
+        return response($response,201);
+    }
+    public function DestroyUser(Request $request){
+        $id=$request->id;
+        User::query()->where('id', $id)->delete();
+    }
 }
